119b1eded3d70bce04887aab0bdcfa3b61ae8c4d
flypoodles <lchen230@uic.edu>
2023-10-16 22:06:55 -0500

    add card class

---
+++ src/main/java/Card.java
@@ -0,0 +1,11 @@
+public class Card {
+
+    public String suite;
+    public int value;
+    Card(String theSuite, int theValue)  {
+        this.suite = theSuite;
+        this.value = theValue;
+    }
+}
+
+
